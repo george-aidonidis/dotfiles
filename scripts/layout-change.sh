9e04405847f3a9ebcc886361f39f5c398d2fda75
George Aidonidis <george.aidonidis@gmail.com>
2018-01-03 19:40:34 +0200

    Add scripts for i3
    * Add lock screen based on i3lock
    * Add two screen layout script.
    * Add layout script to change between gr and us.
    * Swap Ctrl with Alt and Caps with Esc.

---
+++ scripts/layout-change.sh
@@ -0,0 +1,9 @@
+LANG1="us"
+LANG2="gr"
+
+CURRENT_LANG=$(setxkbmap -query | grep "layout" | cut -f6 -d ' ')
+if [ "$CURRENT_LANG" = $LANG1 ]; then
+    setxkbmap $LANG2
+else
+    setxkbmap $LANG1
+fi
---
+++ scripts/lock.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Require `scrot`, `imagemagick`, `i3lock`
+icon="$HOME/dotfiles/config/images/key-lock.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+
+if [[ -f "$icon" ]]
+then
+  # placement x/y
+  PX=0
+  PY=0
+  # lockscreen image info
+  R=$(file "$icon" | grep -o '[0-9]* x [0-9]*')
+  RX=$(echo $R | cut -d' ' -f 1)
+  RY=$(echo $R | cut -d' ' -f 3)
+
+  SR=$(xrandr --query | grep ' connected' | sed 's/primary //' | cut -f3 -d' ')
+  for RES in $SR
+  do
+    # monitor position/offset
+    SRX=$(echo $RES | cut -d'x' -f 1)                   # x pos
+    SRY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 1)  # y pos
+    SROX=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 2) # x offset
+    SROY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 3) # y offset
+    PX=$(($SROX + $SRX/2 - $RX/2))
+    PY=$(($SROY + $SRY/2 - $RY/2))
+
+    convert "$tmpbg" "$icon" -geometry +$PX+$PY -composite -matte "$tmpbg"
+    echo "done"
+  done
+fi
+
+i3lock -e -u -n -i "$tmpbg"
+
+# convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+# i3lock -u -i "$tmpbg"
