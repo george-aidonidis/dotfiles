fa3988f0a206d80127787ff43bcc4d9928c19862
George Aidonidis <george.aidonidis@gmail.com>
2018-03-21 18:05:40 +0200

    Add docker-stop-all

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -37,6 +37,8 @@ alias update="sudo pacman --noconfirm -Syu && yaourt --aur -Syu --noconfirm"
 #########
 alias docker-delete-containers="docker rm \$(docker ps -a -q)"
 alias docker-delete-images="docker rmi \$(docker images -a -q) -f"
+alias docker-stop-all="docker stop $(docker ps -a -q)"
+
 ########
 # Misc #
 ########
