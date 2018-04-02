fb3bc729532bb7fc3c53b5fb55d8365aa60781e1
George Aidonidis <george.aidonidis@gmail.com>
2018-04-02 11:22:26 +0300

    Update install and layout script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #/!bin/bash
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+white=$'\e[0m'
+yellow=$'\e[0;33m'
+
 ## declare an array variable
 declare -a arr=("stow" "compton" "dunst" "i3" "nvim" "polybar" "ranger" "redshift" "rofi" "terminator" "tmux" "zsh")
 
@@ -7,14 +15,14 @@ declare -a arr=("stow" "compton" "dunst" "i3" "nvim" "polybar" "ranger" "redshif
 for item in "${arr[@]}"
 do
 	if ! [ -x "$(command -v $item)" ]; then
-		echo "Error: $item is not installed." >&2
+		echo "$red  Error: $item is not installed.$white" >&2
 		exit 1
 	fi
 	# or do whatever with individual element of the array
 done
 
 echo "Will use stow to symlink:"
-echo "- compton"
+echo "$grn- compton"
 echo "- dunst"
 echo "- i3"
 echo "- idea"
@@ -25,11 +33,12 @@ echo "- ranger"
 echo "- redshift"
 echo "- rofi"
 echo "- terminator"
-echo "- tmux"
+echo "- tmux$white"
 
-echo "Press Ctrl+C to quit now, or Enter to continue"
+printf "\nPress$red Ctrl+C$white to quit now, or$blu Enter$white to continue"
 read -p ""
 
+# TODO: check if one of the folders bellow already exists
 stow compton
 stow dunst
 stow i3
@@ -42,3 +51,13 @@ stow redshift
 stow rofi
 stow terminator
 stow tmux
+
+echo "$grn $white Symlinked folders with stow"
+
+gsettings set  org.gnome.desktop.wm.preferences button-layout 'close,maximize,minimize:'
+echo "$grn $white Changed position of window buttons to left side (gnome)"
+
+echo "$yellow$white"
+echo "  Installing tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm<Paste>
+sudo pacman -S polybar tmux rofi compton neovim redshift dunst terminator gpicview jq peek
--- scripts/layout-change.sh
+++ scripts/layout-change.sh
@@ -2,8 +2,10 @@ LANG1="us"
 LANG2="gr"
 
 CURRENT_LANG=$(setxkbmap -query | grep "layout" | cut -f6 -d ' ')
+sh $HOME/dotfiles/.xinitrc
 if [ "$CURRENT_LANG" = $LANG1 ]; then
 	setxkbmap $LANG2
 else
 	setxkbmap $LANG1
 fi
+
