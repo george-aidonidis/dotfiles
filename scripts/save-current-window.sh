5f4e765302f93a4bc39c053e6c832a5952baed98
George Aidonidis <george.aidonidis@gmail.com>
2018-07-21 21:49:22 +0300

    Change any date `:` to `-`
    close #23

--- scripts/save-current-window.sh
+++ scripts/save-current-window.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILENAME="$HOME/Pictures/screenshots/screenshot$(date +%F_%T).png"
+FILENAME="$HOME/Pictures/screenshots/screenshot$(date +%Y-%m-%d_%H-%M-%S).png"
 
 maim -i $(xdotool getactivewindow) $FILENAME
 notify-send "Saved focused window to $FILENAME"
