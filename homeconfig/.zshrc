5e01e003cc5dc8e0808bab66d45cc1b8d0cdbea2
George Aidonidis <george.aidonidis@gmail.com>
2019-06-22 09:46:31 +0300

    chore: Update polybar
    * Add more monitors to polybar
    * Change workspaces to icons only

--- homeconfig/.zshrc
+++ homeconfig/.zshrc
@@ -24,7 +24,7 @@ export UPDATE_ZSH_DAYS=3
 # Set nvim as a global editor
 export EDITOR='nvim'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export MONITOR='eDP-1'
+export MONITOR='eDP1'
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export QT_SCALE_FACTOR=0
 export UPDATE_ZSH_DAYS=3
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HAS_SECOND_SCREEN=$(xrandr | grep -w "DP1 connected")
+HAS_SECOND_SCREEN=$(xrandr | grep -w "DP1-8 connected")
 # Terminate already running bar instances
 killall -q polybar
 
@@ -10,9 +10,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 
 if [ -n "$HAS_SECOND_SCREEN" ] || [ ! "$HAS_SECOND_SCREEN" = "" ]; then
-  MONITOR="DP1" polybar --reload main &
+  MONITOR="DP1-8" polybar --reload main &
   sleep 0.1
   MONITOR="eDP1" polybar --reload laptop &
+  sleep 0.1
+  MONITOR="DP1-1" polybar --reload port &
 else
 	polybar --reload fallback &
 fi
