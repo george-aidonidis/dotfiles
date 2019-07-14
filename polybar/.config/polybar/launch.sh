320eb3a49a38a4cdb00221eadb9f2d09b5f3618b
George Aidonidis <george.aidonidis@gmail.com>
2019-07-14 13:44:58 +0300

    feat: Make polybar launch a bit dunamic
    close #79

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,20 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-HAS_SECOND_SCREEN=$(xrandr | grep -w "DP1-8 connected")
+SCREENS=$(xrandr | grep " connected " | awk '{ print$1 }')
+
+ACTIVESCREENS=$(echo "$SCREENS" | wc -w)
+
+getConnectedDisplay() {
+	for item in $SCREENS
+	do
+    if [ "$item" != "eDP1" ]; then
+      echo "$item"
+    fi
+	done
+}
+
+startPolybarStatic() {
+  if [ "$ACTIVESCREENS" = "1" ]; then
+    echo "1"
+    MAINMONITOR="$SCREENS" polybar --reload main &
+  elif [ "$ACTIVESCREENS" = "2" ]; then
+    MONITOR="eDP1" polybar --reload laptop &
+    sleep 0.1
+    MAINMONITOR=getConnectedDisplay polybar --reload main &
+  elif [ "$ACTIVESCREENS" = "3" ]; then
+    MONITOR="eDP1" polybar --reload laptop &
+    sleep 0.1
+    MAINMONITOR="DP1-8" polybar --reload main &
+    sleep 0.1
+    SIDEMONITOR="DP1-1" polybar --reload main &
+  else
+    MAINMONITOR="eDP1" polybar --reload main &
+  fi
+}
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+startPolybarStatic
 
-if [ -n "$HAS_SECOND_SCREEN" ] || [ ! "$HAS_SECOND_SCREEN" = "" ]; then
-  MONITOR="DP1-8" polybar --reload main &
-  sleep 0.1
-  MONITOR="eDP1" polybar --reload laptop &
-  sleep 0.1
-  MONITOR="DP1-1" polybar --reload port &
-else
-	polybar --reload fallback &
-fi
