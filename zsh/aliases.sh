a06ba2d8e76330024c99c036ca0d527ccc7ccff7
George Aidonidis <george.aidonidis@gmail.com>
2020-02-25 23:22:16 +0200

    chore: add tmux alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -135,6 +135,48 @@ alias "xv=xclip -o"
 # Sudo edit
 alias "sn=sudo -E nvim"
 
+function tmux() {
+  emulate -L zsh
+
+  # Make sure even pre-existing tmux sessions use the latest SSH_AUTH_SOCK.
+  # (Inspired by: https://gist.github.com/lann/6771001)
+  local SOCK_SYMLINK=~/.ssh/ssh_auth_sock
+  if [ -r "$SSH_AUTH_SOCK" -a ! -L "$SSH_AUTH_SOCK" ]; then
+    ln -sf "$SSH_AUTH_SOCK" $SOCK_SYMLINK
+  fi
+
+  # If provided with args, pass them through.
+  if [[ -n "$@" ]]; then
+    env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux "$@"
+    return
+  fi
+
+  # Check for .tmux file (poor man's Tmuxinator).
+  if [ -x .tmux ]; then
+    # Prompt the first time we see a given .tmux file before running it.
+    local DIGEST="$(openssl sha -sha512 .tmux)"
+    if ! grep -q "$DIGEST" ~/..tmux.digests 2> /dev/null; then
+      cat .tmux
+      read -k 1 -r \
+        'REPLY?Trust (and run) this .tmux file? (t = trust, otherwise = skip) '
+      echo
+      if [[ $REPLY =~ ^[Tt]$ ]]; then
+        echo "$DIGEST" >> ~/..tmux.digests
+        ./.tmux
+        return
+      fi
+    else
+      ./.tmux
+      return
+    fi
+  fi
+
+  # Attach to existing session, or create one, based on current directory.
+  local SESSION_NAME=$(basename "${$(pwd)//[.:]/_}")
+  env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux new -A -s "$SESSION_NAME"
+}
+
+alias "t=tmux"
 alias "tmux-dev= tmux split-window -h -p 70 \; split-window -p 18"
 alias "e=ranger"
 alias "npm-show-global=npm list -g --depth=0"
